9ff8dd0d27fe6118ff35c86cdf36c025a8376669
AudraKenney <audrakkenney@gmail.com>
2019-06-27 19:08:54 -0400

    Initial Commit

---
+++ ARCapstoneTests/ARCapstoneTests.swift
@@ -0,0 +1,34 @@
+//
+//  ARCapstoneTests.swift
+//  ARCapstoneTests
+//
+//  Created by Audra Kenney on 6/27/19.
+//  Copyright © 2019 Audra Kenney. All rights reserved.
+//
+
+import XCTest
+@testable import ARCapstone
+
+class ARCapstoneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
