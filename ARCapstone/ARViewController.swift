8e54c668853d69d29cb61ca6c6136e633d9deb71
Rachel Reinauer <rereinauer@gmail.com>
2019-06-28 17:09:27 -0400

    proof of concept, green thumb

--- ARCapstone/ARViewController.swift
+++ ARCapstone/ARViewController.swift
@@ -11,10 +11,19 @@ import SceneKit
 import ARKit
 import AVFoundation
 
+struct ImageInformation {
+    let name: String
+    let description: String
+    let image: UIImage
+}
+
+
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var selectedImage : ImageInformation?
     
+    let images = ["mdmX" : ImageInformation(name: "Madame X", description: "Portrait of Madame X is the title of a portrait painting by John Singer Sargent of a young socialite, Virginie Amélie Avegno Gautreau, wife of the French banker Pierre Gautreau. Madame X was painted not as a commission, but at the request of Sargent. It is a study in opposition. Sargent shows a woman posing in a black satin dress with jeweled straps, a dress that reveals and hides at the same time. The portrait is characterized by the pale flesh tone of the subject contrasted against a dark colored dress and background. The scandal resulting from the painting's controversial reception at the Paris Salon of 1884 amounted to a temporary set-back to Sargent while in France, though it may have helped him later establish a successful career in Britain and America.", image: UIImage(named: "ColormdmX")!)]
     
     let captureSession = AVCaptureSession()
     var previewLayer: CALayer!
@@ -32,85 +41,84 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
-        // Create a new scene
+//         Create a new scene
         let shipScene = SCNScene(named: "art.scnassets/ship.scn")!
         
         
-//        ShipNode = shipScene.rootNode
-        
-        // Set the scene to the view
+//         Set the scene to the view
         sceneView.scene = shipScene
-        
-//        prepareCamera()
-    }
-    
-//    func prepareCamera() {
-//        captureSession.sessionPreset = AVCaptureSession.Preset.photo
-//
-//        if let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices {
-//            captureDevice = availableDevices.first
-//            beginSession()
-//        }
-//    }
-//
-//    func beginSession() {
-//        do {
-//            let captureDeviceInput = try AVCaptureDeviceInput(device: captureDevice)
-//            captureSession.addInput(captureDeviceInput)
-//        } catch {
-//            print(error.localizedDescription)
-//        }
-//
-//        if let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)?
-//    }
-
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         guard let arImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else { return }
         
         configuration.detectionImages = arImages
         
         // Run the view's session
         sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+    }
+    
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        
     }
     
 
     // MARK: - ARSCNViewDelegate
     
-    func view(_ view: ARSCNView, nodeFor anchor: ARAnchor) -> SCNNode? {
-        if let imageAnchor = anchor as? ARImageAnchor,
-            let referenceImageName = imageAnchor.referenceImage.name,
-            let scannedImage = self.images[referenceImageName] {
-            self.selectedImage = scannedImage
-            
-            self.performSegue(withIdentifier: "showImageInfo", sender: self)
-            
-            return imageSeenMarker()
-        }
-        return nil
-    }
+//    func view(_ view: ARSCNView, nodeFor anchor: ARAnchor) -> SKNode? {
+//        if let imageAnchor = anchor as? ARImageAnchor,
+//            let referenceImageName = imageAnchor.referenceImage.name,
+//            let scannedImage = self.images[referenceImageName] {
+//            self.selectedImage = scannedImage
+//
+//            self.performSegue(withIdentifier: "showImageInfo", sender: self)
+//
+//            return imageSeenMarker()
+//        }
+//        return nil
+//    }
     
-    private func imageSeenMarker() -> SCN {
-        let labelNode = SCNNode(text: "✅")
-        labelNode.horizontalAlignmentMode
-    }
+//    private func imageSeenMarker() -> SKLabelNode {
+//        let labelNode = SKLabelNode(text: "✅")
+//        labelNode.horizontalAlignmentMode = .center
+//        labelNode.verticalAlignmentMode = .center
+//
+//        return labelNode
+//    }
     
 
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        guard anchor is ARImageAnchor else { return }
+        if let imageAnchor = anchor as? ARImageAnchor,
+            let referenceImageName = imageAnchor.referenceImage.name,
+        let scannedImage = self.images[referenceImageName] {
+            self.selectedImage = scannedImage
+            self.performSegue(withIdentifier: "showImageInfo", sender: self)
+            guard let ship = sceneView.scene.rootNode.childNode(withName: "ship", recursively: false) else { return }
+            ship.removeFromParentNode()
+            node.addChildNode(ship)
+            ship.isHidden = false
         
-        // Container
-        guard let ship = sceneView.scene.rootNode.childNode(withName: "ship", recursively: false) else { return }
-        ship.removeFromParentNode()
-        node.addChildNode(ship)
-        ship.isHidden = false
+        }
+ 
         
+        
+
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showImageInfo" {
+            if let imageInformationVC = segue.destination as? ImageInformationViewController,
+                let actualSelectedImage = selectedImage {
+                imageInformationVC.imageInformation = actualSelectedImage
+            }
+        }
     }
 
     
--- ARCapstone/ImageInformationViewController.swift
+++ ARCapstone/ImageInformationViewController.swift
@@ -9,13 +9,6 @@
 import UIKit
 
 class ImageInformationViewController: UIViewController {
-
-    struct ImageInformation {
-        let name: String
-        let description: String
-        let image: UIImage
-    }
-    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var infoText: UITextView!
@@ -24,26 +17,19 @@ class ImageInformationViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      
         
         if let actualImageInformation = imageInformation {
             self.nameLabel.text = actualImageInformation.name
             self.imageView.image = actualImageInformation.image
             self.infoText.text = actualImageInformation.description
         }
+        print(imageInformation)
     }
     
     @IBAction func closeModal(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
