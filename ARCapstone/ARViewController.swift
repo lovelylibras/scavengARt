5549c1050d7ed8030bf51826ec0c417cc55a3f7c
Rachel Reinauer <rereinauer@gmail.com>
2019-07-03 17:20:06 -0400

    plane working with downloaded images

--- ARCapstone/ARViewController.swift
+++ ARCapstone/ARViewController.swift
@@ -8,11 +8,11 @@ var images : [String: UIImage] = [:]
 var visitedNames : [String] = []
 var visitedImages : [UIImage] = []
 
-class ViewController: UIViewController, ARSCNViewDelegate, URLSessionDownloadDelegate {
+class ViewController: UIViewController, ARSCNViewDelegate  {
     @IBOutlet var sceneView: ARSCNView!
-    let imageFetchingGroup = DispatchGroup()
+    var thumbNode: SCNNode?
     var selectedImage : [Paintings]?
-    var index = 1
+   
     let captureSession = AVCaptureSession()
     var previewLayer: CALayer!
     var captureDevice: AVCaptureDevice!
@@ -22,18 +22,23 @@ class ViewController: UIViewController, ARSCNViewDelegate, URLSessionDownloadDel
         
         // Set the view's delegate
         sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
+        let thumbScene = SCNScene(named: "art.scnassets/success.scn")
+        thumbNode = thumbScene?.rootNode
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
 //         Create a new scene
-        let successScene = SCNScene(named: "art.scnassets/success.scn")!
+//        let successScene = SCNScene(named: "art.scnassets/success.scn")!
+//
+////         Set the scene to the view
+//        sceneView.scene = successScene
         
-//         Set the scene to the view
-        sceneView.scene = successScene
-        
-        imageFetchingGroup.enter()
-        downloadImageTask(media:arrOfArt)
+        let configuration = ARWorldTrackingConfiguration()
+        let detectionImages = loadedImagesFromDirectoryContents()
+        configuration.detectionImages = detectionImages
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
 
     }
 
@@ -41,12 +46,45 @@ class ViewController: UIViewController, ARSCNViewDelegate, URLSessionDownloadDel
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        imageFetchingGroup.notify(queue: .main) {
-            let configuration = ARWorldTrackingConfiguration()
-            let detectionImages = self.loadedImagesFromDirectoryContents()
-            configuration.detectionImages = detectionImages
-            self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+  
+        
+        
+    }
+    
+    func loadedImagesFromDirectoryContents() -> Set<ARReferenceImage>?{
+        var index = 0
+        var customReferenceSet = Set<ARReferenceImage>()
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentsDirectory = paths[0]
+        do {
+            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsDirectory, includingPropertiesForKeys: nil, options: [])
+            let filteredContents = directoryContents.filter{ $0.pathExtension == "png" }
+            filteredContents.forEach { (url) in
+                do{
+                    //1. Create A Data Object From Our URL
+                    let imageData = try Data(contentsOf: url)
+                    guard let image = UIImage(data: imageData) else { return }
+                    //2. Convert The UIImage To A CGImage
+                    guard let cgImage = image.cgImage else { return }
+                    //3. Get The Width Of The Image
+                    let imageWidth = CGFloat(cgImage.width)
+                    //4. Create A Custom AR Reference Image With A Unique Name
+                    let customARReferenceImage = ARReferenceImage(cgImage, orientation: CGImagePropertyOrientation.up, physicalWidth: imageWidth)
+                    customARReferenceImage.name = "MyCustomARImage\(index)"
+                    //4. Insert The Reference Image Into Our Set
+                    customReferenceSet.insert(customARReferenceImage)
+                    print("ARReference Image == \(customARReferenceImage)")
+                    index += 1
+                }catch{
+                    print("Error Generating Images == \(error)")
+                }
+            }
+        } catch {
+            print("Error Reading Directory Contents == \(error)")
         }
+        //5. Return The Set
+        print("HELLOOOOOO")
+        return customReferenceSet
     }
     
 //    func addReferences(media: [Paintings]) {
@@ -92,105 +130,52 @@ class ViewController: UIViewController, ARSCNViewDelegate, URLSessionDownloadDel
 //            self.sceneView.session.run(self.configuration)
 //        }
 //    }
+
     
     
     
-    func downloadImageTask(media: [Paintings]){
-        
-        for medium in media {
-            let imageUrl = medium.imageUrl
-            //1. Get The URL Of The Image
-            guard let url = URL(string: imageUrl) else {
-                return
-            }
-            //2. Create The Download Session
-            let downloadSession = URLSession(configuration: URLSession.shared.configuration, delegate: self as? URLSessionDelegate, delegateQueue: nil)
-            //3. Create The Download Task & Run It
-            let downloadTask = downloadSession.downloadTask(with: url)
-            downloadTask.resume()
-            print("My image url:", imageUrl)
-        }
-        print("1")
-    }
     
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        imageFetchingGroup.enter()
-        //1. Create The Filename
-        let fileURL = getDocumentsDirectory().appendingPathComponent("image\(index).png")
-        //2. Copy It To The Documents Directory
-        do {
-            try FileManager.default.copyItem(at: location, to: fileURL)
-            print("Successfuly Saved File \(fileURL)")
-            index = index + 1
-        } catch {
-            print("Error Saving: \(error)")
-        }
-        print("3")
-    }
     
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        let documentsDirectory = paths[0]
-        print("2")
-        return documentsDirectory
-    }
-    
-    /// Creates A Set Of ARReferenceImages From All PNG Content In The Documents Directory
-    ///
-    /// - Returns: Set<ARReferenceImage>
-    func loadedImagesFromDirectoryContents() -> Set<ARReferenceImage>?{
-        var index = 0
-        var customReferenceSet = Set<ARReferenceImage>()
-        let documentsDirectory = getDocumentsDirectory()
-        do {
-            let directoryContents = try FileManager.default.contentsOfDirectory(at: documentsDirectory, includingPropertiesForKeys: nil, options: [])
-            let filteredContents = directoryContents.filter{ $0.pathExtension == "png" }
-            filteredContents.forEach { (url) in
-                do{
-                    //1. Create A Data Object From Our URL
-                    let imageData = try Data(contentsOf: url)
-                    guard let image = UIImage(data: imageData) else { return }
-                    //2. Convert The UIImage To A CGImage
-                    guard let cgImage = image.cgImage else { return }
-                    //3. Get The Width Of The Image
-                    let imageWidth = CGFloat(cgImage.width)
-                    //4. Create A Custom AR Reference Image With A Unique Name
-                    let customARReferenceImage = ARReferenceImage(cgImage, orientation: CGImagePropertyOrientation.up, physicalWidth: imageWidth)
-                    customARReferenceImage.name = "MyCustomARImage\(index)"
-                    //4. Insert The Reference Image Into Our Set
-                    customReferenceSet.insert(customARReferenceImage)
-                    print("ARReference Image == \(customARReferenceImage)")
-                    index += 1
-                }catch{
-                    print("Error Generating Images == \(error)")
-                }
-            }
-        } catch {
-            print("Error Reading Directory Contents == \(error)")
-        }
-        //5. Return The Set
-        print("HELLOOOOOO")
-        return customReferenceSet
-    }
     
     // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if let imageAnchor = anchor as? ARImageAnchor,
-        let referenceImageName = imageAnchor.referenceImage.name {
-            let scannedImage = arrOfArt.filter({$0.name == referenceImageName})
-            self.selectedImage = scannedImage
-//            self.performSegue(withIdentifier: "showImageInfo", sender: self)
-            guard let thumb = sceneView.scene.rootNode.childNode(withName: "thumb", recursively: false) else { return }
-//            thumb.removeFromParentNode()
-            node.addChildNode(thumb)
-            thumb.isHidden = false
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            let size = imageAnchor.referenceImage.physicalSize
+            let plane = SCNPlane(width: size.width, height: size.height)
+            plane.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
+            plane.cornerRadius = 0.005
+            let planeNode = SCNNode(geometry: plane)
+            planeNode.eulerAngles.x = -.pi / 2
+            node.addChildNode(planeNode)
             
-//            if !visitedNames.contains(referenceImageName) {
-//                visitedNames.append(referenceImageName)
-//                visitedImages.append(images[referenceImageName]!)
-//            }
+            if let shapeNode = thumbNode {
+                node.addChildNode(shapeNode)
+            }
+              print("I recognize this image")
         }
-        print("recognized the image!")
+        
+      
+        return node
+        
+        
+//        let referenceImageName = imageAnchor.referenceImage.name {
+//            let scannedImage = arrOfArt.filter({$0.name == referenceImageName})
+//            self.selectedImage = scannedImage
+////            self.performSegue(withIdentifier: "showImageInfo", sender: self)
+//            guard let thumb = sceneView.scene.rootNode.childNode(withName: "thumb", recursively: false) else { return }
+//            thumb.removeFromParentNode()
+//            node.addChildNode(thumb)
+//            thumb.isHidden = false
+//            print(thumb.isHidden)
+//
+////            if !visitedNames.contains(referenceImageName) {
+////                visitedNames.append(referenceImageName)
+////                visitedImages.append(images[referenceImageName]!)
+////            }
+//        }
+//        print("recognized the image!")
     }
     
 //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
